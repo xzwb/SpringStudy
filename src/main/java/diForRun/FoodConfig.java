1ad8af2e25c2a13c59105963aa8ac34255171c8d
xzwb <1478863479@qq.com>
2020-02-23 11:13:25 +0800

    使用配置文件运行时注入

--- src/main/java/diForRun/FoodConfig.java
+++ src/main/java/diForRun/FoodConfig.java
@@ -1,13 +1,20 @@
 package diForRun;
 
 import diForRun.impl.Apple;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource("classpath:/apple.properties")
 public class FoodConfig {
+    @Autowired
+    Environment environment;
+
     @Bean
     public Food apple() {
-        return new Apple("apple");
+        return new Apple(environment.getProperty("apple.foodName"));
     }
 }
