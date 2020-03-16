a0d815d4eb91b64681895c7e4ae9bd6354467289
xzwb <1478863479@qq.com>
2020-03-16 16:16:58 +0800

    注解使用properties文件

--- src/main/java/diForRun/FoodConfig.java
+++ src/main/java/diForRun/FoodConfig.java
@@ -2,6 +2,7 @@ package diForRun;
 
 import diForRun.impl.Apple;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,11 +11,18 @@ import org.springframework.core.env.Environment;
 @Configuration
 @PropertySource("classpath:/apple.properties")
 public class FoodConfig {
-    @Autowired
-    Environment environment;
+//    @Autowired
+//    Environment environment;
+//
+//    @Bean
+//    public Food apple() {
+//        return new Apple(environment.getProperty("apple.foodName"));
+//    }
+    @Value("${apple.foodName}")
+    private String name;
 
     @Bean
     public Food apple() {
-        return new Apple(environment.getProperty("apple.foodName"));
+        return new Apple(name);
     }
 }
